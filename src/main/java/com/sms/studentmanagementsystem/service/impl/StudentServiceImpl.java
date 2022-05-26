bba80e0b731b7900883994d3ef7b20cfdbc63097
vkotsiuba99 <92172262+vkotsiuba99@users.noreply.github.com>
2022-05-26 15:48:19 +0300

    add create, edit student

--- src/main/java/com/sms/studentmanagementsystem/service/impl/StudentServiceImpl.java
+++ src/main/java/com/sms/studentmanagementsystem/service/impl/StudentServiceImpl.java
@@ -19,4 +19,19 @@ public class StudentServiceImpl implements StudentService {
     public List<Student> getAllStudents() {
         return studentRepository.findAll();
     }
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public Student getStudentById(Long id) {
+        return studentRepository.findById(id).get();
+    }
+
+    @Override
+    public Student updateStudent(Student student) {
+        return studentRepository.save(student);
+    }
 }
